24512ca61c0f6c54345d71d3cf8f42c588fed1a7
bbarr <bbarr1384@gmail.com>
2016-02-19 17:42:02 +0100

    test for https://github.com/bbarr/medium/issues/3

---
+++ test/bugs.js
@@ -0,0 +1,29 @@
+
+import 'babel-polyfill'
+import assert from 'assert'
+import { chan, put, sleep, go, any } from '../lib/index'
+
+
+describe('bugs reports', () => {
+
+  describe('https://github.com/bbarr/medium/issues/3', () => {
+
+    it('should resolve ch1 put of 123', (cb) => {
+
+      const channel = chan()
+
+      go(async () => {
+
+        const ch1 = chan()
+        const p = sleep(100)
+
+        put(ch1, 123)
+
+        const [ v, ch ] = await any(ch1, p)
+
+        assert.equal(v, 123)
+      }).then(cb)
+    })
+  })
+})
+
